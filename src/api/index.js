8711e71ba3f7c2d7fcd69e299a9e24987ddc2302
IBN5102 programmer <minhnghcm123@gmail.com>
2021-03-17 14:17:15 +0000

    Updated API and updated app.js to link new data

--- src/api/index.js
+++ src/api/index.js
@@ -5,16 +5,80 @@ const endpoint = (
     'https://covid19.mathdro.id/api'
 );
 
+<<<<<<< HEAD
 export const fetchData = async () =>  {
     
+=======
+const url = (
+    'https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nation;areaName=england'
+);
+
+export const fetchData = async () => {
+>>>>>>> ad7fec6 (Updated API and updated app.js to link new data)
     try {
         const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(endpoint);
-        
         const sctructuredData = { confirmed, recovered, deaths, lastUpdate }
+
+        // const { data } = await axios.get(endpoint);
+        // const sctructuredData = { data }
 
         return sctructuredData;
 
     } catch (error) {
+        console.log(error);
+    }
+}
+
+// (IBN) Using the same API file to fetch data for Graph page
+function endpointGraph() {
+    const
+        AreaType = "nation",
+        AreaName = "england";
+    const
+        filters = [
+            `areaType=${AreaType}`,
+            `areaName=${AreaName}`
+        ],
+        structure = {
+            date: "date",
+            name: "areaName",
+            cases: {
+                daily: "newCasesByPublishDate",
+                cumulative: "cumCasesByPublishDate"
+            },
+            deaths: {
+                daily: "newDeaths28DaysByPublishDate",
+                cumulative: "cumDeaths28DaysByPublishDate"
+            }
+        };
+    // const
+    //     apiParams = `filters=${filters.join(";")}&structure=${JSON.stringify(structure)}`,
+    //     encodedParams = encodeURI(apiParams);
+    // return `https://api.coronavirus.data.gov.uk/v1/data?${encodedParams}`;
+    const
+        apiParams = {
+            filters: filters.join(";"),
+            structure: JSON.stringify(structure),
+            // latestBy: "newCasesByPublishDate"
+        };
+    return apiParams;
+}
+
+export const graphData = async () => {
+    try {
+        const endpoint = 'https://api.coronavirus.data.gov.uk/v1/data';
+
+        const { data: {data} } = await axios.get(
+            endpoint, {
+            params: endpointGraph(),
+            timeout: 10000
+            }
+        );
+        const sctructuredData = data;
+        return sctructuredData;
+
+    } catch (error) {
+        console.log("Testing");
         console.log(error);
     }
 }
--- src/components/pages/Graph/Graph.tsx
+++ src/components/pages/Graph/Graph.tsx
@@ -3,23 +3,30 @@ import React from 'react';
 import Chart from '../../Chart/Chart';
 import './Graph.css';
 
-const Graph: React.FC = () => (
-    <IonPage>
+const Graph: React.FC = (props) => (
 
-        <IonHeader>
-            <IonToolbar mode="ios">
-                <IonTitle>
-                    Graph page
-                </IonTitle>
+    <IonApp>
+        <IonPage>
+            <IonToolbar>
+                <IonButtons slot="start">
+                    <IonBackButton defaultHref="/home" />
+                </IonButtons>
+                <IonTitle>Local graph of COVID</IonTitle>
             </IonToolbar>
         </IonHeader>
 
         <IonContent>
             
+<<<<<<< HEAD
 
         </IonContent>
     </IonPage>
 
+=======
+            <Chart data={props}></Chart>
+        </IonPage>
+    </IonApp>
+>>>>>>> ad7fec6 (Updated API and updated app.js to link new data)
 );
 
 export default Graph;
