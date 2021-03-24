99a6e36e8e15d82a68f3d75d2c687ff0126c6137
IBN5102 programmer <minhnghcm123@gmail.com>
2021-03-24 16:25:23 +0000

    Updated Chart.jsx to v3

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
+import { IonContent } from '@ionic/react';
 
 const Chart = (data) => {
     const { data: { props } } = data;
@@ -14,18 +15,37 @@ const Chart = (data) => {
 
     var areaName = new Array(dataArray.length);
     var areaCumulativeCases = new Array(dataArray.length);
+    var areaDailyCases = new Array(dataArray.length);
+    var areaCumulativeDeaths = new Array(dataArray.length);
+    var areaDailyDeaths = new Array(dataArray.length);
     for (var i = 0; i < dataArray.length; i++) {
         areaName[i] = dataArray[i][0].name;
         areaCumulativeCases[i] = new Array(innerLength);
+        areaDailyCases[i] = new Array(innerLength);
+        areaCumulativeDeaths[i] = new Array(innerLength);
+        areaDailyDeaths[i] = new Array(innerLength);
         for (var j = 0; j < innerLength; j++) {
             areaCumulativeCases[i][j] = [
                 new Date(dataArray[i][j].date).getTime(),
                 dataArray[i][j].cases.cumulative
             ];
+            areaDailyCases[i][j] = [
+                new Date(dataArray[i][j].date).getTime(),
+                dataArray[i][j].cases.daily
+            ];
+            areaCumulativeDeaths[i][j] = [
+                new Date(dataArray[i][j].date).getTime(),
+                dataArray[i][j].deaths.cumulative
+            ];
+            areaDailyDeaths[i][j] = [
+                new Date(dataArray[i][j].date).getTime(),
+                dataArray[i][j].deaths.daily
+            ];
         }
     }
-    console.log(areaName);
-    console.log(areaCumulativeCases);
+    // console.log(areaName);
+    // console.log(areaCumulativeCases);
+
     // var dataArray = data.data.props;
     // var areaEngland = dataArray[0].name;
     // var casesEngland = new Array(dataArray.length);
@@ -40,7 +60,8 @@ const Chart = (data) => {
     // }
     // console.log(casesEngland);
 
-    const options = {
+    // (IBN) Graph options for cumulative cases by area
+    const optionsGraph1 = {
         chart: {
             type: 'line'
         },
@@ -88,10 +109,164 @@ const Chart = (data) => {
         ]
     };
 
+    // (IBN) Graph options for daily cases by area
+    const optionsGraph2 = {
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'COVID-19 daily cases by area'
+        },
+        xAxis: {
+            type: 'datetime',
+            min: minDate,
+            max: maxDate,
+            title: {
+                text: 'Time'
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Daily cases'
+            }
+        },
+        series: [
+            {
+                // (IBN) England
+                name: areaName[0],
+                type: undefined,
+                data: areaDailyCases[0]
+            },
+            {
+                // (IBN) Scotland
+                name: areaName[1],
+                type: undefined,
+                data: areaDailyCases[1]
+            },
+            {
+                // (IBN) Wales
+                name: areaName[2],
+                type: undefined,
+                data: areaDailyCases[2]
+            },
+            {
+                // (IBN) Northern Ireland
+                name: areaName[3],
+                type: undefined,
+                data: areaDailyCases[3]
+            }
+        ]
+    };
+
+    // (IBN) Graph options for cumulative deaths by area
+    const optionsGraph3 = {
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'COVID-19 cumulative deaths by area'
+        },
+        xAxis: {
+            type: 'datetime',
+            min: minDate,
+            max: maxDate,
+            title: {
+                text: 'Time'
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Cummulative deaths'
+            }
+        },
+        series: [
+            {
+                // (IBN) England
+                name: areaName[0],
+                type: undefined,
+                data: areaCumulativeDeaths[0]
+            },
+            {
+                // (IBN) Scotland
+                name: areaName[1],
+                type: undefined,
+                data: areaCumulativeDeaths[1]
+            },
+            {
+                // (IBN) Wales
+                name: areaName[2],
+                type: undefined,
+                data: areaCumulativeDeaths[2]
+            },
+            {
+                // (IBN) Northern Ireland
+                name: areaName[3],
+                type: undefined,
+                data: areaCumulativeDeaths[3]
+            }
+        ]
+    };
+
+    // (IBN) Graph options for daily deaths by area
+    const optionsGraph4 = {
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'COVID-19 daily deaths by area'
+        },
+        xAxis: {
+            type: 'datetime',
+            min: minDate,
+            max: maxDate,
+            title: {
+                text: 'Time'
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Daily deaths'
+            }
+        },
+        series: [
+            {
+                // (IBN) England
+                name: areaName[0],
+                type: undefined,
+                data: areaDailyDeaths[0]
+            },
+            {
+                // (IBN) Scotland
+                name: areaName[1],
+                type: undefined,
+                data: areaDailyDeaths[1]
+            },
+            {
+                // (IBN) Wales
+                name: areaName[2],
+                type: undefined,
+                data: areaDailyDeaths[2]
+            },
+            {
+                // (IBN) Northern Ireland
+                name: areaName[3],
+                type: undefined,
+                data: areaDailyDeaths[3]
+            }
+        ]
+    };
+
     return(
-        <div id="container">
-            <HighchartsReact highcharts={Highcharts} options={options} />
-        </div>
+        // <div id="container">
+        //     <HighchartsReact highcharts={Highcharts} options={optionsGraph1} />
+        //     <HighchartsReact highcharts={Highcharts} options={optionsGraph2} />
+        // </div>
+        <IonContent id="container">
+            <HighchartsReact highcharts={Highcharts} options={optionsGraph1} />
+            <HighchartsReact highcharts={Highcharts} options={optionsGraph2} />
+            <HighchartsReact highcharts={Highcharts} options={optionsGraph3} />
+            <HighchartsReact highcharts={Highcharts} options={optionsGraph4} />
+        </IonContent>
     )
 }
 
